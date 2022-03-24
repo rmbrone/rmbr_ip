e114c4fc893556da965f3b369983434a2aff80b3
aman <modamankhan@gmail.com>
2022-03-24 11:24:09 +0530

    fix: added null safety

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nikk.li/Work/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wenyan/zeno/project/flutter-projects/flutter_ip/flutter_ip/example"
+export "FLUTTER_ROOT=/Users/Macky/Macky/applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Macky/Downloads/delp/rmbr_ip/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/nikk.li/Work/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
--- lib/flutter_ip.dart
+++ lib/flutter_ip.dart
@@ -5,18 +5,33 @@ import 'package:flutter/services.dart';
 class FlutterIp {
   static const MethodChannel _channel = const MethodChannel('flutter_ip');
 
-  static Future<String> get networkType async {
-    final String type = await _channel.invokeMethod('getNetworkType');
-    return type;
+  static Future<String?> get networkType async {
+    try {
+      final String? type = await _channel.invokeMethod('getNetworkType');
+      return type;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 
-  static Future<String> get internalIP async {
-    final String ip = await _channel.invokeMethod('getInternalIP');
-    return ip;
+  static Future<String?> get internalIP async {
+    try {
+      final String? ip = await _channel.invokeMethod('getInternalIP');
+      return ip;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 
-  static Future<String> get externalIP async {
-    final String ip = await _channel.invokeMethod('getExternalIP');
-    return ip;
+  static Future<String?> get externalIP async {
+    try {
+      final String? ip = await _channel.invokeMethod('getExternalIP');
+      return ip;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
